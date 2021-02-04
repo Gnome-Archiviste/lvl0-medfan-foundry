5dbbc88462547f9b770a6c0d54793eca5e4656c9
Socolin <provost.bertrand@gmail.com>
2021-02-03 20:45:46 -0500

    Add new handlebar operator for addition

---
+++ src/scripts/handlebars.js
@@ -0,0 +1,12 @@
+
+Handlebars.registerHelper('math', function (lvalue, operator, rvalue) {
+    rvalue = parseFloat(rvalue);
+    return {
+        "+": lvalue + rvalue,
+        "-": lvalue - rvalue,
+        "*": lvalue * rvalue,
+        "/": lvalue / rvalue,
+        "%": lvalue % rvalue
+    }[operator];
+});
+
