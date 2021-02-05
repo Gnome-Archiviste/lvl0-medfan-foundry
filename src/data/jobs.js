0cc2c197e7dd990d84eb67103200a48f6dd2c058
Philippe Gamache <philippe.gamache@lightspeedhq.com>
2021-02-04 21:42:04 -0500

    Update Jobs

---
+++ src/data/jobs.js
@@ -0,0 +1,46 @@
+export default {
+    base: {
+        champion: {
+            name: "Champion",
+            section: "champion"
+        },
+        ranger: {
+            name: "Forestier",
+            section: "ranger"
+        },
+        warrior: {
+            name: "Guerrier",
+            section: "warrior"
+        },
+        mage: {
+            name: "Magicien",
+            section: "mage"
+        },
+        rogue: {
+            name: "Voleur",
+            section: "rogue"
+        }
+    },
+    advance: {
+        jester: {
+            name: "Bouffon",
+            section: "mage"
+        },
+        elementalist: {
+            name: "Élémentaliste",
+            section: "mage"
+        },
+        enchanter: {
+            name: "Enchanteur",
+            section: "mage"
+        },
+        necromancer: {
+            name: "Nécromancien",
+            section: "mage"
+        },
+        witch: {
+            name: "Sorcière",
+            section: "mage"
+        }
+    }
+}
