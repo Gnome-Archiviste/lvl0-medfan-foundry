092c7a7b4ae5d4fcec700f99f33714806df9c67e
Philippe Gamache <philippe.gamache@lightspeedhq.com>
2021-02-04 21:12:54 -0500

    Update Classes Skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -2,7 +2,91 @@ export default {
     champion: {
         shield_attack: {
             name: "Attaque de bouclier",
+            stat: "phy"
+        },
+        commandment: {
+            name: "Commandement",
+            stat: "cha"
+        },
+        disarm: {
+            name: "Désarmer",
+            stat: "dex"
+        },
+        detect_motivation: {
+            name: "Détection motivation",
             stat: "int"
         }
-    }
+    },
+    ranger: {
+        stealth: {
+            name: "Camouflage",
+            stat: "dex"
+        },
+        move_silently: {
+            name: "Déplacement silencieux",
+            stat: "dex"
+        },
+        amimal_taming: {
+            name: "Maîtrise des animaux",
+            stat: "int"
+        },
+        tracking: {
+            name: "Pister",
+            stat: "per"
+        }
+    },
+    warrior: {
+        charge: {
+            name: "Charge",
+            stat: "phy"
+        },
+        two_handed_combat: {
+            name: "Combat à deux mains",
+            stat: "phy"
+        },
+        intimidation: {
+            name: "Intimidation",
+            stat: "cha"
+        },
+        provocation: {
+            name: "Provocation",
+            stat: "cha"
+        }
+    },
+    mage: {
+        focus: {
+            name: "Concentration",
+            stat: "int"
+        },
+        knowledge: {
+            name: "Connnaissance",
+            stat: "int"
+        },
+        magic_item: {
+            name: "Création d'items magiques",
+            stat: "dex"
+        },
+        detect_magic: {
+            name: "Détection de la magie",
+            stat: "per"
+        }
+    },
+    rogue: {
+        move_silently: {
+            name: "Déplacement silencieux",
+            stat: "dex"
+        },
+        appraisal: {
+            name: "Évaluation",
+            stat: "int"
+        },
+        forgery: {
+            name: "Faux-semblant",
+            stat: "int"
+        },
+        larceny: {
+            name: "Larcin",
+            stat: "dex"
+        }
+    },
 }
