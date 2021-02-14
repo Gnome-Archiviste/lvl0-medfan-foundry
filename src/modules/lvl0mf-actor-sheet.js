784d6dfb745dd4daf065c80cd39a3aff5e19214d
Socolin <provost.bertrand@gmail.com>
2021-02-14 15:08:15 -0500

    Add button near items in actor inventory to be able to delete/edit items

--- src/modules/lvl0mf-actor-sheet.js
+++ src/modules/lvl0mf-actor-sheet.js
@@ -17,12 +17,35 @@ export class Lvl0mfActorSheet extends ActorSheet {
     /** @override */
     getData(options) {
         const data = super.getData(options);
-
         let jobsNamesById = Object.entries(jobs.base).reduce(((previousValue, currentValue) => {
             previousValue[currentValue[0]] = currentValue[1].name;
             return previousValue;
         }), {})
 
         return {...data, skills, jobs: jobs.base, jobsNamesById: jobsNamesById}
+    }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        // Update Inventory Item
+        html.find('.item-edit').click(ev => {
+            const itemLine = $(ev.currentTarget).parents(".item-line");
+            const item = this.actor.getOwnedItem(itemLine.data("item-id"));
+            if (item) {
+                item.sheet.render(true);
+            }
+        });
+
+        // Delete Inventory Item
+        html.find('.item-delete').click(ev => {
+            const itemLine = $(ev.currentTarget).parents(".item-line");
+            const item = this.actor.getOwnedItem(itemLine.data("item-id"));
+            Dialog.confirm({
+                title: 'Supression', content: 'Voulez vous supprimer ' + item.name, yes: () => {
+                    this.actor.deleteOwnedItem(itemLine.data("item-id"));
+                }
+            });
+        });
     }
 }
