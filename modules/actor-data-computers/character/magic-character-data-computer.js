a123903079656d809316d0a12626386934c036e4
Socolin <provost.bertrand@gmail.com>
2022-01-02 08:29:08 -0500

    Support arcane 20

--- modules/actor-data-computers/character/magic-character-data-computer.js
+++ modules/actor-data-computers/character/magic-character-data-computer.js
@@ -9,7 +9,9 @@ export class MagicCharacterDataComputer extends CharacterDataComputer {
         let arcaneLevels = actorData.computedData.bases.job?.arcaneLevels;
         let arcaneLevel = 0;
 
-        if (arcaneLevels) {
+        if (actorData.level.value >= 70)
+            arcaneLevel = 20;
+        else if (arcaneLevels) {
             for (let i = arcaneLevels.length - 1; i >= 0; i--) {
                 if (actorData.level.value >= arcaneLevels[i]) {
                     arcaneLevel = i + 1;
