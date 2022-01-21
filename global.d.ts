ed86baab2b1dfb3263a01d29c18e6973731c8499
Socolin <provost.bertrand@gmail.com>
2022-01-21 03:40:36 -0500

    fix typo

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,3 @@
-import {ConfiguredDocumentClass} from '@league-of-foundry-developers/foundry-vtt-types/src/types/helperTypes';
-
 declare interface RollTerm {
     results: DiceTerm.Result[];
-}
-
-declare global {
-    // @ts-ignore
 }
