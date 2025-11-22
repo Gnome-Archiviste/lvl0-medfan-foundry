8f027d99848c0f41a3d11035519f3d59589a5a74
Socolin <provost.bertrand@gmail.com>
2025-11-21 21:26:44 -0500

    Update list of stats used by actor

--- src/models/shared/stat-names.ts
+++ src/models/shared/stat-names.ts
@@ -1,4 +1,4 @@
 export const ActorBasicStatNamesList = ['phy', 'dex', 'int', 'cha', 'per',] as const;
-export const ActorStatNamesList = [...ActorBasicStatNamesList, 'protection', 'health', 'mana', 'movement'] as const;
+export const ActorStatNamesList = [...ActorBasicStatNamesList, 'protection', 'health', 'mana', 'movement', 'damage'] as const;
 export type ActorBasicStatNames = typeof ActorBasicStatNamesList[number];
 export type ActorStatNames = typeof ActorStatNamesList[number];
